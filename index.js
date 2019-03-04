acf7d594ffc9487f9f9880b7781d42e2ece4c53f
Ben Botvinick <benbotvinick@gmail.com>
2019-03-04 14:59:27 -0500

    1.1.0

--- index.js
+++ index.js
@@ -6,68 +6,89 @@ const concat = require('concat');
 const fs = require('fs');
 const path = require('path');
 const args = require('args');
+const git = require('simple-git');
+const glob = require('glob');
+const isOnline = require('is-online');
 
 inquirer.registerPrompt('search-checkbox', require('inquirer-search-checkbox'));
 
 function init() {
-    const title = fs.readFileSync(path.join(__dirname, 'title.txt'));
-    console.log(
-        chalk.white('\n' + title + '\n')
-    );
+	const title = fs.readFileSync(path.join(__dirname, 'title.txt'));
+	console.log(
+		chalk.white('\n' + title + '\n')
+	);
+}
+
+async function updateTemplates() {
+	if (!(await isOnline())) return;
+
+	console.log(
+		chalk.blue('Updating templates...')
+	);
+
+	if (!fs.existsSync('gitignore')) git().clone('https://github.com/github/gitignore', path.join(__dirname, 'gitignore'));
+	else git(path.join(__dirname, 'gitignore')).pull();
+
+	glob(path.join(__dirname, 'gitignore/**/*.gitignore'), (err, files) => {
+		for (let file of files) {
+			fs.copyFileSync(file, path.join('templates', path.basename(file)));
+		}
+	});
 }
 
 function askQuestions() {
-    const questions = [
-        {
-            type: 'search-checkbox',
-            name: 'files',
-            message: 'Which gitignores would you like to use?',
-            choices: fs.readdirSync(path.join(__dirname, 'templates')).map(x => x.replace('.gitignore', ''))
-        },
-        {
-            type: 'confirm',
-            name: 'confirmation',
-            message: `Create .gitignore file in ${process.cwd()}?`
-        }
-    ];
-    return inquirer.prompt(questions);
+	const questions = [
+		{
+			type: 'search-checkbox',
+			name: 'files',
+			message: 'Which gitignores would you like to use?',
+			choices: fs.readdirSync(path.join(__dirname, 'templates')).map(x => x.replace('.gitignore', ''))
+		},
+		{
+			type: 'confirm',
+			name: 'confirmation',
+			message: `Create .gitignore file in ${process.cwd()}?`
+		}
+	];
+	return inquirer.prompt(questions);
 }
 
 function success() {
-    console.log(
-        chalk.green.bold(`Created .gitignore file in ${process.cwd()}`)
-    );
+	console.log(
+		chalk.green.bold(`Created .gitignore file in ${process.cwd()}`)
+	);
 }
 
 function parseArgs() {
-    const config = {
-        name: 'ignore',
-        mainColor: 'white'
-    };
+	const config = {
+		name: 'ignore',
+		mainColor: 'white'
+	};
 
-    return args.parse(process.argv, config)
+	return args.parse(process.argv, config);
 }
 
-async function run() {
-    // parse arguments
-    const flags = parseArgs();
+(async () => {
+	// parse arguments
+	const flags = parseArgs();
 
-    // show script introduction
-    init();
+	// show script introduction
+	init();
 
-    // ask questions
-    const answers = await askQuestions();
-    const {files, confirmation} = answers;
+	// update templates
+	await updateTemplates();
 
-    if (confirmation) {
-        concat(files.map(x => path.join(__dirname, 'templates', x + '.gitignore')))
-            .then((result) => {
-                fs.writeFileSync(`${process.cwd()}/.gitignore`, result);
-            });
+	// ask questions
+	const answers = await askQuestions();
+	const {files, confirmation} = answers;
 
-        // show success message
-        success();
-    }
-}
+	if (confirmation) {
+		concat(files.map(x => path.join(__dirname, 'templates', x + '.gitignore')))
+			.then((result) => {
+				fs.writeFileSync(`${process.cwd()}/.gitignore`, result);
+			});
 
-run();
+		// show success message
+		success();
+	}
+})();
